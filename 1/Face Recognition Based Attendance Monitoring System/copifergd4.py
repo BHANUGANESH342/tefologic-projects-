e7e2bf10a15c590d4b731a0edd5e3443061b776d
CH B T G V Prasad <121245836+BHANUGANESH342@users.noreply.github.com>
2023-12-28 11:29:38 +0530

    Add files via upload

---
+++ 1/Face Recognition Based Attendance Monitoring System/copifergd4.py
@@ -0,0 +1,271 @@
+import tkinter as tk
+from tkinter import ttk
+from tkinter import messagebox as mess
+import tkinter.simpledialog as tsd
+import cv2, os
+import csv
+import numpy as np
+from PIL import Image
+import pandas as pd
+import datetime
+import time
+
+def assure_path_exists(path):
+    dir = os.path.dirname(path)
+    if not os.path.exists(dir):
+        os.makedirs(dir)
+
+def tick():
+    time_string = time.strftime('%H:%M:%S')
+    clock.config(text=time_string)
+    clock.after(200, tick)
+
+def contact():
+    mess._show(title='Contact us', message="Please contact us on : 'shubhamkumar8180323@gmail.com' ")
+
+def check_haarcascadefile():
+    exists = os.path.isfile("haarcascade_frontalface_default.xml")
+    if not exists:
+        mess._show(title='Some file missing', message='Please contact us for help')
+        window.destroy()
+
+def save_pass():
+    assure_path_exists("TrainingImageLabel/")
+    exists1 = os.path.isfile("TrainingImageLabel\psd.txt")
+    if exists1:
+        tf = open("TrainingImageLabel\psd.txt", "r")
+        key = tf.read()
+    else:
+        master.destroy()
+        new_pas = tsd.askstring('Old Password not found', 'Please enter a new password below', show='*')
+        if new_pas == None:
+            mess._show(title='No Password Entered', message='Password not set!! Please try again')
+        else:
+            tf = open("TrainingImageLabel\psd.txt", "w")
+            tf.write(new_pas)
+            mess._show(title='Password Registered', message='New password was registered successfully!!')
+            return
+
+    op = (old.get())
+    newp = (new.get())
+    nnewp = (nnew.get())
+    if op == key:
+        if newp == nnewp:
+            txf = open("TrainingImageLabel\psd.txt", "w")
+            txf.write(newp)
+        else:
+            mess._show(title='Error', message='Confirm new password again!!!')
+            return
+    else:
+        mess._show(title='Wrong Password', message='Please enter correct old password.')
+        return
+    mess._show(title='Password Changed', message='Password changed successfully!!')
+    master.destroy()
+
+def change_pass():
+    global master
+    master = tk.Tk()
+    master.geometry("400x160")
+    master.resizable(False, False)
+    master.title("Change Password")
+    master.configure(background="white")
+    lbl4 = tk.Label(master, text='    Enter Old Password', bg='white', font=('comic', 12, ' bold '))
+    lbl4.place(x=10, y=10)
+    global old
+    old = tk.Entry(master, width=25, fg="black", relief='solid', font=('comic', 12, ' bold '), show='*')
+    old.place(x=180, y=10)
+    lbl5 = tk.Label(master, text='   Enter New Password', bg='white', font=('comic', 12, ' bold '))
+    lbl5.place(x=10, y=45)
+    global new
+    new = tk.Entry(master, width=25, fg="black", relief='solid', font=('comic', 12, ' bold '), show='*')
+    new.place(x=180, y=45)
+    lbl6 = tk.Label(master, text='Confirm New Password', bg='white', font=('comic', 12, ' bold '))
+    lbl6.place(x=10, y=80)
+    global nnew
+    nnew = tk.Entry(master, width=25, fg="black", relief='solid', font=('comic', 12, ' bold '), show='*')
+    nnew.place(x=180, y=80)
+    cancel = tk.Button(master, text="Cancel", command=master.destroy, fg="black", bg="red", height=1, width=25,
+                       activebackground="white", font=('comic', 10, ' bold '))
+    cancel.place(x=200, y=120)
+    save1 = tk.Button(master, text="Save", command=save_pass, fg="black", bg="#00fcca", height=1, width=25,
+                      activebackground="white", font=('comic', 10, ' bold '))
+    save1.place(x=10, y=120)
+    master.mainloop()
+
+def psw():
+    assure_path_exists("TrainingImageLabel/")
+    exists1 = os.path.isfile("TrainingImageLabel\psd.txt")
+    if exists1:
+        tf = open("TrainingImageLabel\psd.txt", "r")
+        key = tf.read()
+    else:
+        new_pas = tsd.askstring('Old Password not found', 'Please enter a new password below', show='*')
+        if new_pas == None:
+            mess._show(title='No Password Entered', message='Password not set!! Please try again')
+        else:
+            tf = open("TrainingImageLabel\psd.txt", "w")
+            tf.write(new_pas)
+            mess._show(title='Password Registered', message='New password was registered successfully!!')
+            return
+
+    password = tsd.askstring('Password', 'Enter Password', show='*')
+    if password == key:
+        TrainImages()
+    elif password == None:
+        pass
+    else:
+        mess._show(title='Wrong Password', message='You have entered the wrong password')
+
+def clear():
+    txt.delete(0, 'end')
+    res = "1)Take Images  >>>  2)Save Profile"
+    message1.configure(text=res)
+def clear2():
+    txt2.delete(0, 'end')
+    res = "1)Take Images  >>>  2)Save Profile"
+    message1.configure(text=res)
+
+def TakeImages():
+    check_haarcascadefile()
+    columns = ['SERIAL NO.', '', 'ID', '', 'NAME']
+    assure_path_exists("StudentDetails/")
+    assure_path_exists("TrainingImage/")
+    serial = 0
+    exists = os.path.isfile("StudentDetails\StudentDetails.csv")
+    if exists:
+        with open("StudentDetails\StudentDetails.csv", 'r') as csvFile1:
+            reader1 = csv.reader(csvFile1)
+            for l in reader1:
+                serial = serial + 1
+        serial = (serial // 2)
+        csvFile1.close()
+    else:
+        with open("StudentDetails\StudentDetails.csv", 'a+') as csvFile1:
+            writer = csv.writer(csvFile1)
+            writer.writerow(columns)
+            serial = 1
+        csvFile1.close()
+    Id = (txt.get())
+    name = (txt2.get())
+    if name.isalpha() or (' ' in name):
+        cam = cv2.VideoCapture(0)
+        harcascadePath = "haarcascade_frontalface_default.xml"
+        detector = cv2.CascadeClassifier(harcascadePath)
+        sampleNum = 0
+        while True:
+            ret, img = cam.read()
+            gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+            faces = detector.detectMultiScale(gray, 1.3, 5)
+            for (x, y, w, h) in faces:
+                cv2.rectangle(img, (x, y), (x + w, y + h), (255, 0, 0), 2)
+                sampleNum = sampleNum + 1
+                cv2.imwrite("TrainingImage\ " + name + "." + Id + '.' + str(serial) + "." + str(sampleNum) + ".jpg",
+                            gray[y:y + h, x:x + w])
+                cv2.imshow('Frame', img)
+            if cv2.waitKey(100) & 0xFF == ord('q'):
+                break
+            elif sampleNum > 60:
+                break
+        cam.release()
+        cv2.destroyAllWindows()
+        res = "Images Saved for ID : " + Id + " Name : " + name
+        row = [serial, '', Id, '', name]
+        with open('StudentDetails\StudentDetails.csv', 'a+') as csvFile:
+            writer = csv.writer(csvFile)
+            writer.writerow(row)
+        csvFile.close()
+        message1.configure(text=res)
+    else:
+        if name.isalpha():
+            res = "Enter Alphabetical Name"
+            message.configure(text=res)
+
+        if name == "":
+            res = "Enter the name"
+            message.configure(text=res)
+
+        if not name.isalpha():
+            res = "Enter Alphabetical Name"
+            message.configure(text=res)
+
+
+def TrainImages():
+    recognizer = cv2.face.LBPHFaceRecognizer_create()
+    assure_path_exists("TrainingImageLabel/")
+    detector = cv2.CascadeClassifier("haarcascade_frontalface_default.xml")
+    faces, Id = getImagesAndLabels("TrainingImage")
+    recognizer.train(faces, np.array(Id))
+    recognizer.save("TrainingImageLabel\Trainner.yml")
+    res = "Image Trained"  # +",".join(str(f) for f in Id)
+    message1.configure(text=res)
+
+def getImagesAndLabels(path):
+    # get the path of all the files in the folder
+    imagePaths = [os.path.join(path, f) for f in os.listdir(path)]
+    # create empth face list
+    faces = []
+    # create empty ID list
+    Ids = []
+    # now looping through all the image paths and loading the Ids and the images
+    for imagePath in imagePaths:
+        # loading the image and converting it to gray scale
+        pilImage = Image.open(imagePath).convert('L')
+        # Now we are converting the PIL image into numpy array
+        imageNp = np.array(pilImage, 'uint8')
+        # getting the Id from the image
+        Id = int(os.path.split(imagePath)[-1].split(".")[1])
+        # extract the face from the training image sample
+        faces.append(imageNp)
+        Ids.append(Id)
+    return faces, Ids
+
+def TrackImages():
+    check_haarcascadefile()
+    recognizer = cv2.face.LBPHFaceRecognizer_create()
+    recognizer.read("TrainingImageLabel\Trainner.yml")
+    harcascadePath = "haarcascade_frontalface_default.xml"
+    faceCascade = cv2.CascadeClassifier(harcascadePath)
+    df = pd.read_csv("StudentDetails\StudentDetails.csv")
+    cam = cv2.VideoCapture(0)
+    font = cv2.FONT_HERSHEY_SIMPLEX
+    col_names = ['Id', 'Name', 'Date', 'Time']
+    attendance = pd.DataFrame(columns=col_names)
+    while True:
+        ret, im = cam.read()
+        gray = cv2.cvtColor(im, cv2.COLOR_BGR2GRAY)
+        faces = faceCascade.detectMultiScale(gray, 1.2, 5)
+        for (x, y, w, h) in faces:
+            cv2.rectangle(im, (x, y), (x + w, y + h), (0, 260, 0), 7)
+            Id, conf = recognizer.predict(gray[y:y + h, x:x + w])
+            if conf < 70:
+                aa = df.loc[df['Id'] == Id]['Name'].values
+                tt = str(Id) + "-" + aa
+            else:
+                Id = 'Unknown'
+                tt = str(Id)
+            if (100 < conf):
+                noOfFile = len(os.listdir("ImagesUnknown")) + 1
+                cv2.imwrite("ImagesUnknown\Image" + str(noOfFile) + ".jpg", im[y:y + h, x:x + w])
+            else:
+                tt = str(Id) + "-" + aa
+            if (conf > 75):
+                noOfFile = len(os.listdir("ImagesUnknown")) + 1
+                cv2.imwrite("ImagesUnknown\Image" + str(noOfFile) + ".jpg", im[y:y + h, x:x + w])
+                tt = str(Id)
+            cv2.putText(im, str(tt), (x, y + h), font, 1, (255, 255, 255), 2)
+            attendance.loc[len(attendance)] = [Id, aa, str(datetime.datetime.now().date()), str(
+                datetime.datetime.now().time())]
+        attendance = attendance.drop_duplicates(subset=['Id'], keep='first')
+        cv2.imshow('im', im)
+        if (cv2.waitKey(1) == ord('q')):
+            break
+    ts = time.time()
+    date = datetime.datetime.fromtimestamp(ts).strftime('%Y-%m-%d')
+    timeStamp = datetime.datetime.fromtimestamp(ts).strftime('%H:%M:%S')
+    Hour, Minute, Second = timeStamp.split(":")
+    fileName = "Attendance\Attendance_" + date + "_" + Hour + "-" + Minute + "-" + Second + ".csv"
+    attendance.to_csv(fileName, index=False)
+    cam.release()
+    cv2.destroyAllWindows()
+    res = attendance
+    message2.configure(text=res)
